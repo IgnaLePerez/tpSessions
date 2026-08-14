3d1ab6a0bec959f2bb19b4f2534582e289a1f74b
IgnaLePerez <49961152@est.ort.edu.ar>
2026-08-13 21:57:55 -0300

    Terminado

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,2 +1,15 @@
 ﻿const inputContraseña = document.getElementById("contraseña");
+const msjContraseña = document.getElementById("msjContraseña");
 
+function verificarContraseña(){
+    console.log("entró")
+    let contraseñaIngresada = document.getElementById("contraseña").value;
+
+    if (contraseñaIngresada.length < 8){
+        inputContraseña.style.borderColor = "red";
+        msjContraseña.innerHTML = "La contraseña debe tener como mínimo 8 caracteres";
+        msjContraseña.style.color = "red";
+        return false
+    }
+    return true
+}
